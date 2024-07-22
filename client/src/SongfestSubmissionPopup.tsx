f4414861c43bbf771ffe6ac7863a21c32b247335
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2024-07-22 11:39:06 -0400

    comments

--- client/src/SongfestSubmissionPopup.tsx
+++ client/src/SongfestSubmissionPopup.tsx
@@ -13,18 +13,28 @@ function SongfestSubmissionPopup() {
     return (
         <>
             <form>
+                {/* info about the combobox here: https://headlessui.com/react/combobox */}
+                {/* the combobox is connected to the participant state variable and updates it when an option is selected */}
+                {/* the combobox also sets the query to "" when closing the box (clears the text)*/}
                 <p>Name:</p>
                 <Combobox value={participant} onChange={setParticipant} onClose={() => setQuery("")} immediate>
+                    {/* the query state variable updates to match what the user types */}
                     <ComboboxInput onChange={(event) => setQuery(event.target.value)} displayValue={(person: string) => person}/>
                     <ComboboxOptions anchor="bottom start">
+                        {/* dynamically create an option based off what the user is typing. */}
+                        {/* this option will show if the query is not whitespace and if the query does not match an existing participant*/}
                         {query.trim().length > 0 && !SongfestStatus.participants.includes(query) && (
                         <ComboboxOption value={query}>
                             Create <span className="font-bold">"{query}"</span>
                         </ComboboxOption>
                         )}
-                        {SongfestStatus.participants.filter((person) => {
+
+                        {// filter the list of participants using the query
+                        SongfestStatus.participants.filter((person) => {
                             return person.toLowerCase().includes(query.toLowerCase())
-                        }).map((person, index) => (
+                        })
+                        // for every participant remaining, create an option for them in the combobox
+                        .map((person, index) => (
                             <ComboboxOption value={person} key={index}>
                                 {person}
                             </ComboboxOption>
@@ -33,6 +43,7 @@ function SongfestSubmissionPopup() {
                 </Combobox>
                 <br></br>
 
+                {/* create inputs for song urls. the number of inputs is determined by SongfestStatus.songsPerPerson */}
                 {Array.from({length: SongfestStatus.songsPerPerson}).map((_, index) => {
                     return (
                         <div key={"song" + (index + 1)}>
@@ -42,6 +53,7 @@ function SongfestSubmissionPopup() {
                     )
                 })}
 
+                {/* the submit button currently just does preventDefault to stop the default action of submitting the form (which reloads the page) */}
                 <button onSubmit={(event) => {
                     event.preventDefault()
                     return false
