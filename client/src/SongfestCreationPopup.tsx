5c6cff145ab36a0e6e304574a1f797db13adc4eb
Kevin Li <luoshuyi1124@gmail.com>
2024-07-30 11:51:31 -0400

    a test for pull request

--- client/src/SongfestCreationPopup.tsx
+++ client/src/SongfestCreationPopup.tsx
@@ -9,7 +9,7 @@ function SongfestCreationPopup() {
 
     return (
         <>
-            <form>
+            <form className='popup'>
                 <label htmlFor="theme">Theme:</label> <br></br>
                 <input type="text" id="theme" required
                 onChange={(event) => {
