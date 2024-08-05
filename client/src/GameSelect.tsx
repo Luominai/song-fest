c1c143b8ee1cada2318ccc604e09398bc41780ac
Kevin Li <luoshuyi1124@gmail.com>
2024-08-05 10:08:53 -0400

    made wip player select screen

--- client/src/GameSelect.tsx
+++ client/src/GameSelect.tsx
@@ -1,7 +1,20 @@
+import { useContext } from "react"
+import GameContext from "./GameContext"
+
 export default function GamePlayerSelect() {
+    const gameState = useContext(GameContext);
+    const participants = ["jfeioab", "my name is jeff", "diddy kong"]
     return (
         <>
-
+            <form className="playerSelect">
+                {participants.map((name) => {
+                    return (
+                        <button>
+                            {name}
+                        </button>
+                    )
+                })}
+            </form>
         </>
     )
 }
