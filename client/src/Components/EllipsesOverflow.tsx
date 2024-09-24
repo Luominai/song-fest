b4bdea5fb7caa3e420e533fb27f58ab414c81655
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2024-09-24 01:19:43 -0400

    i made an ellipses overflow thing because the built in one didn't work

--- client/src/Components/EllipsesOverflow.tsx
+++ client/src/Components/EllipsesOverflow.tsx
@@ -3,13 +3,48 @@ import { useEffect, useRef, useState } from "react"
 export default function EllipsesOverflow({text, maxHeight, width}: {text: string, maxHeight: number, width: number}) {
     const [innerText, setInnerText] = useState(text)
     const element = useRef<HTMLSpanElement>()
+    const toggle = useRef(false)
+    const left = useRef(0)
+    const middle = useRef(text.length)
+    const right = useRef(text.length)
 
     useEffect(() => {
         if (!element.current?.getBoundingClientRect()) {
             return
         }
         if (element.current?.getBoundingClientRect().height > maxHeight) {
-            setInnerText("overflow")
+            toggle.current = true;
+        }
+    },[])
+
+    useEffect(() => {
+        if (!element.current?.getBoundingClientRect()) {
+            return
+        }
+        if (!toggle) {
+            return
+        }
+        // if the pointers cross or overlap, stop
+        if (right >= left) {
+            toggle.current = false
+            setInnerText(text.slice(0, middle.current - 3) + "...")
+        }
+        // search for the right position to put the ellipses
+        if (element.current?.getBoundingClientRect().height > maxHeight) {
+            // move the right pointer to this position
+            right.current = middle.current
+            // change the middle to somewhere in the middle
+            middle.current = Math.floor((left.current + right.current) / 2)
+            // add ellipses at the middle and try it
+            setInnerText(text.slice(0, middle.current) + "...")
+        }
+        if (element.current.getBoundingClientRect().height < maxHeight) {
+            // move the left pointer to this position
+            left.current = middle.current
+            // change the middle to somewhere in the middle
+            middle.current = Math.floor((left.current + right.current) / 2)
+            // add ellipses at the middle and try it
+            setInnerText(text.slice(0, middle.current) + "...")
         }
     })
 
